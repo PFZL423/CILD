0860753ff60d34829227a6d9f945b40a2f28c3ec
PFZL423 <YHA0112@sjtu.edu.cn>
2026-05-09 11:39:52 +0800

    video save fix

--- tdmpc2/run_shaping.sh
+++ tdmpc2/run_shaping.sh
@@ -17,7 +17,7 @@ run() {
         task=$task \
         cost_lambda=$lam \
         steps=500000 model_size=1 seed=1 \
-        enable_wandb=false eval_freq=5000 eval_episodes=1 \
+        enable_wandb=false eval_freq=5000 eval_episodes=1 save_video=false \
         exp_name=shaping_lam${lam} \
         > "$LOG_DIR/${tag}.log" 2>&1 &
     echo $! > "$LOG_DIR/${tag}.pid"
